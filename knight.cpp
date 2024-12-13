0df10b1f1db3ba77fe6e881466ade2ab4953de5c
mihsto632 <smihajlo133@gmail.com>
2024-12-13 11:52:47 +0000

    Added functions for setting up game mode, initializing game state (number of mines, initial positions...)

--- knight.cpp
+++ knight.cpp
@@ -6,12 +6,26 @@
 
 //Board class constructor for initializing a board with blank elements
 Board::Board(){
-    //Initialize all board elements to ' '
+    //Initialize a dynamically allocated board as a matrix of elements
     board = new char*[8];
 
     for (int i=0; i<8; i++){
         board[i] = new char[8];
     }
+
+    //Set initial knight and flag positions
+    initial_knight_x = generate_random_number_1_8();
+    initial_knight_y = generate_random_number_1_8();
+    do{
+        target_flag_x = generate_random_number_1_8();
+        target_flag_y = generate_random_number_1_8();
+    }
+    while ((target_flag_x == initial_knight_x && target_flag_y == initial_knight_y) ||
+           check_move_legality(initial_knight_x, initial_knight_y, target_flag_x, target_flag_y)); 
+
+    //Set initial knight and flag positions by setting 'H' and 'X' on the board
+    board[initial_knight_x-1][initial_knight_y-1] = current_position; // 'H'
+    board[target_flag_x-1][target_flag_y-1]       = flag_position;    // 'X'
 }
 
 //Board class destructor
@@ -26,7 +40,6 @@ Board::~Board(){
 void Board::draw_board(){
     wcout << "\033[2J\033[H\n";
     wcout<<" ___ ___ ___ ___ ___ ___ ___ ___\n";
-    board[0][0] = 'H';
     int count_rows{8};
     for (int i=0; i<8; i++){
         wcout<<"| ";
@@ -59,6 +72,34 @@ void Board::draw_board(){
         wcout << "  A   B   C   D   E   F   G   H\n";
 }
 
+//Function that checks move legality
+bool Board::check_move_legality(int initial_knight_x, int initial_knight_y, int target_flag_x, int target_flag_y){
+    if ((target_flag_x == initial_knight_x+2 && target_flag_y == initial_knight_y+1) ||
+        (target_flag_x == initial_knight_x+2 && target_flag_y == initial_knight_y-1) ||
+        (target_flag_x == initial_knight_x-2 && target_flag_y == initial_knight_y+1) ||
+        (target_flag_x == initial_knight_x-2 && target_flag_y == initial_knight_y-1) ||
+        (target_flag_x == initial_knight_x+1 && target_flag_y == initial_knight_y+2) ||
+        (target_flag_x == initial_knight_x+1 && target_flag_y == initial_knight_y-2) ||
+        (target_flag_x == initial_knight_x-1 && target_flag_y == initial_knight_y+2) ||
+        (target_flag_x == initial_knight_x-1 && target_flag_y == initial_knight_y-2))
+        return true;
+    else 
+        return false;
+}
+//Function that removes a number of mines based on game mode
+void Board::remove_mines(/*int& remove_mines_num, char& mine_position*/){
+    int x{}, y{};
+    for (int i=0; i<remove_mines_num; i++){
+        do{
+            x = generate_random_number_1_8();
+            y = generate_random_number_1_8();
+        }
+        while (board[x-1][y-1] == mine_position);
+        board[x-1][y-1] = ' '; 
+    }
+}
+//Function that generates initial mines
+//void generate_initial_mines(int& initial_mines)
 
 //-------------------------------------
 //Class Figure function implementations
@@ -71,4 +112,69 @@ void Board::draw_board(){
 //function that draws the board
 void Game::draw_board(Board& b){
     b.draw_board();
+}
+//Function that sets the game mode
+void Game::set_game_mode(){
+    wcout << "\033[2J\033[H\n";
+    wcout<<"Select game mode: \n\n\n0 - Tutorial\t 1 - Easy\t 2 - Medium\t 3 - Survival\n\n\n";
+    do {
+        cin>>game_mode;
+    }
+    while (game_mode<0 || game_mode>3);
+    wcout<<"\n\n\n";
+}
+//Function that sets up the variables
+void Game::setup_variables(/*int& game_mode, */Board& b){
+    switch (game_mode){
+        case 0: // TUTORIAL GAME MODE
+            b.initial_mines   = 5;
+            //b.mine_increment_per_move = 1;
+            b.mine_increment_per_flag = 1;
+            b.remove_mines_num = 1;
+            max_moves_allowed = 1000;
+            number_of_rounds  = 1;
+            break;
+        case 1: // EASY GAME MODE
+            b.initial_mines   = 8;
+            //b.mine_increment_per_move = 2;
+            b.mine_increment_per_flag = 2;
+            b.remove_mines_num = 2;
+            max_moves_allowed = 1000;
+            number_of_rounds  = 3;
+            break;
+        case 2: // MEDIUM GAME MODE
+            b.initial_mines   = 12;
+            //b.mine_increment_per_move = 2;
+            b.mine_increment_per_flag = 3;
+            b.remove_mines_num = 3;
+            max_moves_allowed = 1000;
+            number_of_rounds  = 10;
+            break;
+        case 3: // SURVIVAL GAME MODE
+            b.initial_mines   = 5;
+            //b.mine_increment_per_move = 1;
+            b.mine_increment_per_flag = 3;
+            b.remove_mines_num = 5;
+            max_moves_allowed = 10000;
+            number_of_rounds  = 100;
+            break;  
+    }
+}
+//Function that is in charge of making a move
+/*void Game::make_move(char& letter_y, char& letter_x){
+    int next_x, next_y;
+    wcout<<"Enter next valid move: ";
+    cin>>letter_y >> letter_x; //Entering new values that are treated like strings
+
+}*/
+
+
+//--------------------------------
+//Generic function implementations
+//--------------------------------
+// Function to generate a random number between 1 and 8
+int generate_random_number_1_8() {
+    //Seeding random number generator with the current time
+    srand(static_cast<unsigned>(time(0)));
+    return rand() % 8 + 1;
 }
--- knight.h
+++ knight.h
@@ -21,14 +21,22 @@ using namespace std;
 //----------------------------
 class Board{
     private:
-        char** board;
+        char** board; //Pointer to a pointer for the purpose of dynamic allocation 
+        int initial_mines, max_mines_allowed{35}, mine_increment_per_move, mine_increment_per_flag, current_mine_counter, remove_mines_num;
+        int initial_knight_x, initial_knight_y;
+        int target_flag_x, target_flag_y;
+        char current_position = 'H', flag_position = 'X', mine_position = '*';
+        char letter_x, letter_y; //Used as parameters for converting numbers into actual
+                                 //coordinates
     public:
         void draw_board();
-        bool check_board_state();
+        void set_initial_knight_flag_pos();
+        void update_figure_position();
         void generate_initial_mines();
         void generate_additional_mines();
-        void remove_mines();
-        void generate_flags();
+        void remove_mines(/*int& remove_mines_num, char& mine_position*/);
+        void generate_flag();
+        bool check_move_legality(int initial_knight_x, int initial_knight_y, int target_flag_x, int target_flag_y); //When figures other than knight are implemented, type of figure should be a function parameter
         Board();
         ~Board();
         friend class Game;
@@ -40,8 +48,6 @@ class Board{
 class Figure{
     private:
     public:
-        bool check_move_legality(); //When figures other than knight are implemented, type of figure should be a function parameter
-        void make_move(); //Logic resembling if (check_move_legality == true) do_the_move
         bool check_player_input();
         friend class Game;
 };
@@ -50,12 +56,26 @@ class Figure{
 //Initializing the Game class
 //---------------------------
 class Game{
+    private:
+        const char* filename;
+        int flag_counter{0};
+        int move_counter{0};
+        int max_moves_allowed;
+        int successive_illegal_move_counter{0};
+        int number_of_rounds;
     public:
+        int game_mode{0}; //default game mode set to 0, which is Tutorial mode
         void draw_board(Board& b);
         void add_figure(Figure& f); //adds an object of class figure
-        void setup_variables();
+        bool check_game_state(); //if game is over, returns true
+        void setup_variables(/*int& game_mode, */Board& b); //based on game mode, sets initial number of mines, mines increment, and mine generation/removal
         void enter_name();
-        void show_scoreboard();
+        void make_move(); //calls check_board_state from Board class object
+        void show_scoreboard(); //initializes the filename based on game mode
+                                //performs logic based on scoreboard files
+        void set_game_mode(); //ex: tutorial, easy, survival, custom
+            //should take initial_mines from the board object
+
 };
 
 //------------------------------

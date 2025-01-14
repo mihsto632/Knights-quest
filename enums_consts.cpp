886bb5dccabb9e0f18fbdb3653ad08baa1190341
mihsto632 <smihajlo133@gmail.com>
2025-01-14 09:40:12 +0000

    Added a file that contains const declarations for commonly used parameters

---
+++ enums_consts.cpp
@@ -0,0 +1,8 @@
+//Custom data type that stores different kind of chess pieces
+enum enemy{KNIGHT, ROOK, BISHOP, QUEEN, KING};
+const int BOARD_SIZE = 8;
+const char current_position = 'H';
+const char mine_position    = '*';
+const char flag_position    = 'X';
+const char enemy_position   = 'E';
+wchar_t enemy_figure;
